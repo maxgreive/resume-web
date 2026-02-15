1e94b75f13cebe593b5f49d3d9942b161be29dc6
Max Greive <maxgreive@googlemail.com>
2026-02-15 17:47:42 +0100

    update site config

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,9 +5,9 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: 'SRLEOM',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'astro-theme-resume',
+	title: 'Max Greive',
 	// Meta property used as the default description meta property
-	description: 'The official Astro Resume Theme',
+	description: 'Lead Web Developer @ SNOCKS',
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: 'en-GB',
 	// Meta property, found in src/components/BaseHead.astro L:42
@@ -28,10 +28,10 @@ export const menuLinks: Array<{ title: string; path: string }> = [
 		title: 'Home',
 		path: '/'
 	},
-	{
-		title: 'Blog',
-		path: '/blog/'
-	}
+	// {
+	// 	title: 'Blog',
+	// 	path: '/blog/'
+	// }
 ]
 
 // https://expressive-code.com/reference/configuration/
